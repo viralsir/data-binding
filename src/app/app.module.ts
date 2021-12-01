310ba6d1141bd84ee96ce9ba43cf308e1afa810e
ViralSir <viralsir2018@gmail.com>
2021-12-01 17:29:14 +0530

    two binding ,event binding and template ref are added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StringIntropolationComponent } from './string-intropolation/string-intropolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TemplateRefComponent } from './template-ref/template-ref.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringIntropolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    TemplateRefComponent,
+    TwoWayBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
